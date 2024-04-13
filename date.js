1f0272ccd10aa67dc90dafa72b7dd0e0daf4bb51
Blaze5333 <arwachaiwala@gmail.com>
2024-04-13 12:32:13 +0530

    Completed

---
+++ date.js
@@ -0,0 +1,3 @@
+const timestamp = 1712949361481;
+const date = new Date(timestamp);
+console.log(date);
---
+++ index.js
@@ -0,0 +1,53 @@
+const express=require('express')
+const app=express()
+const bodyParser=require('body-parser')
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.json())
+const { google } = require('googleapis');
+const calendar = google.calendar('v3');
+require('dotenv').config()
+const otpGenerator=require('otp-generator')
+const nodemailer=require('nodemailer')
+app.post('/generateOtp',async(req,res)=>{
+  try {
+    const {email}=req.body
+    console.log("email",email)
+    const otp=otpGenerator.generate(4,{specialChars:false,lowerCaseAlphabets:false,upperCaseAlphabets:false,digits:true})
+    console.log("otp",otp)
+    const transport=nodemailer.createTransport({
+      service: 'Gmail',
+      auth: {
+        user: 'mustafachaiwala2003@gmail.com',
+        pass: process.env.EMAIL_PASSWORD
+      }
+    })
+    const mailOptions={
+      from:'mustafachaiwala2003@gmail.com',
+      to:email,
+      subject: 'Here is your one-time-password',
+    text: `Here's the one-time verification code you requested ${otp}.This code expires after 9 minutes`
+    }
+    transport.sendMail(mailOptions,(error,info)=>{
+      if(error){
+        console.log(error)
+      }else{
+        console.log('Email sent: ' + info.response);
+        res.send({message:'otp sent successfully',otp:otp})
+      }
+    })
+    res.json({
+      otp,
+      error:0
+    })
+    } catch (error) {
+    res.json({
+      error:1,
+      message:"Something went wrong"
+    })
+  }
+})
+
+
+app.listen(3000,()=>{
+    console.log('lidtening on 3000')
+})
